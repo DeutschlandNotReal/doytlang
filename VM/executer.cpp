b79f41dd3d0fb92eb47151e95c67b1d2d8cd8a83
DeutschlandNotReal <frenchmapping610@gmail.com>
2025-09-16 20:06:07 +0100

    minor quick VM change

--- VM/executer.cpp
+++ VM/executer.cpp
@@ -11,7 +11,7 @@ void execute(doytVM machine){
             case Opcode::SUB: { machine.reg[instr.c] = machine.reg[instr.a] - machine.reg[instr.b]; break; }
             case Opcode::MUL: { machine.reg[instr.c] = machine.reg[instr.a] * machine.reg[instr.b]; break; }
             case Opcode::DIV: { machine.reg[instr.c] = machine.reg[instr.a] / machine.reg[instr.b]; break; }
-            case Opcode::MOD: { machine.reg[instr.c] = (int)machine.reg[instr.a] % (int)machine.reg[instr.b]; break; }
+            case Opcode::MOD: { machine.reg[instr.c] = std::fmod(machine.reg[instr.a], (int)machine.reg[instr.b]); break; }
 
             case Opcode::SQRT:{ machine.reg[instr.c] = std::sqrt(machine.reg[instr.a]); break; }
             case Opcode::EXP: { machine.reg[instr.c] = std::exp (machine.reg[instr.a]); break; }
@@ -22,6 +22,7 @@ void execute(doytVM machine){
             case Opcode::sSUB:{ machine.reg[instr.c] -= machine.reg[instr.a]; break; }
             case Opcode::sMUL:{ machine.reg[instr.c] *= machine.reg[instr.a]; break; }
             case Opcode::sDIV:{ machine.reg[instr.c] /= machine.reg[instr.a]; break; }
+            case Opcode::sMOD:{ machine.reg[instr.c] = std::fmod(machine.reg[instr.c], machine.reg[instr.a]); }
 
             case Opcode::PRINT:{ std::cout << machine.reg[instr.a]; }
         };
